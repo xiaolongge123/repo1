17c16f632483720ecec79df8e4dd5d2f104f864b
xiaolongge123 <1576809847@qq.com>
2019-08-12 09:12:30 +0800

    添加工程

--- idear_git_test01/src/com/itheima/test01.java
+++ idear_git_test01/src/com/itheima/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println ("我来了");
         System.out.println ("大家都老了");
+        System.out.println ("大佬也来了");
     }
 }
